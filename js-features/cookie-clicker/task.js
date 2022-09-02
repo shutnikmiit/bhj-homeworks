9d4483cd93eebc0c8e4c8c93ae93acb56055d072
shutnikmiit <shutnikmiit@mail.ru>
2022-09-02 10:22:59 +0300

    js browser

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,11 @@
+const cookie = document.getElementById("cookie");
+const clickerCounter = document.getElementById("clicker__counter");
+
+cookie.onclick = function () {
+  let clicks = clickerCounter.textContent++;
+  if (clicks % 2 == 0) {
+    cookie.width = 150;
+  } else {
+    cookie.width = 200;
+  }
+}
