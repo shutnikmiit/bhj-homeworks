4657bf9f08e864b8c85ba89a2f319c174670a9e1
shutnikmiit <shutnikmiit@mail.ru>
2022-09-06 13:41:44 +0300

    secondTask

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,6 +4,8 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
+    this.timerElement = container.querySelector('.status__timer');
+    this.timerId = 0;
 
     this.reset();
 
@@ -17,13 +19,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    window.addEventListener('keydown', (event) => {
+      if (this.currentSymbol.textContent === event.key.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
@@ -56,18 +58,18 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -77,12 +79,26 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
 
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+
+    clearInterval(this.timerId);
+    this.timer();
+  }
+
+  timer() {
+    let counter = this.wordElement.querySelectorAll('.symbol').length;
+
+    this.timerId = setInterval(() => {
+      this.timerElement.innerText = `${counter--}`;
+      if (counter < 0) {
+        this.fail();
+      }
+    }, 1000);
   }
 }
 
